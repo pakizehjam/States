b387047e9a74a3fc9dab07399d4259701a0c48e7
Mohammad Pakizeh Jam <61758931+pakizehjam@users.noreply.github.com>
2021-03-18 00:05:30 -0700

    update

--- states.py
+++ states.py
@@ -4,12 +4,12 @@ from sqlalchemy import create_engine
 hostname="127.0.0.1"
 uname="root"
 pwd=""
-dbname="states"
+dbname="zipcodes"
 
 engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}"
             .format(host=hostname, db=dbname, user=uname, pw=pwd))
 
-tables = pandas.read_csv(r"C:\Users\ellis\Documents\Populationstate.csv")
+tables = pandas.read_csv(r"D:\Drive D\CNA335\week6\zip_code_database.csv")
 
 
 
